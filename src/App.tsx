92da17fb631cf5967685cf3c5006e5dbb0a2c45d
Ngudup <tenzinngodup900@gmail.com>
2024-01-24 23:38:04 +0530

    Deleting data

--- src/App.tsx
+++ src/App.tsx
@@ -30,14 +30,34 @@ const App = () => {
 
     return () => controller.abort();
   }, []);
+  const deleteUser = (user: User) => {
+    const originalUser = [...users];
+    // Update the ui state
+    setUsers(users.filter((u) => u.id !== user.id));
+    // Call the server
+    axios
+      .delete("https://jsonplaceholder.typicode.com/usersx/" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUser);
+      });
+  };
 
   return (
     <>
       {error && <p className="text-danger">{error} </p>}
       {isloading && <p className="text-danger">LOADING ...</p>}
-      <ul>
+      <ul className="list-group">
         {users.map((u) => (
-          <li key={u.id}>{u.name}</li>
+          <li
+            key={u.id}
+            className="list-group-item d-flex justify-content-between align-items-center "
+          >
+            {u.name}
+            <button onClick={() => deleteUser(u)} className="btn btn-danger">
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </>
